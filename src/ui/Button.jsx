e13e6b390ad656cc612cfba3f796bb637f74ed12
M3hTi <mehdinasir502@yahoo.com>
2025-07-16 17:28:19 +0330

    Complete Button ui component.

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,7 +1,18 @@
 import { Link } from "react-router-dom";
 
 function Button({ to = "", children, onClick, className }) {
-  
+  if (to) {
+    return (
+      <Link to={to} className={className}>
+        {children}
+      </Link>
+    );
+  }
+  return (
+    <button className={className} onClick={onClick}>
+      {children}
+    </button>
+  );
 }
 
 export default Button;
