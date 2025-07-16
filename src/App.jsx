fe94ca90baccf2ae19dfc863486d3a92f7dfda99
M3hTi <mehdinasir502@yahoo.com>
2025-07-16 17:07:57 +0330

    ✨ Complete Routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AppLayout from "./ui/AppLayout";
 import Landing from "./pages/Landing";
 import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
           <Route index element={<Landing />} />
           <Route path="/home" element={<Home />} />
         </Route>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
