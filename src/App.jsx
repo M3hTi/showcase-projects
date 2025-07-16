d7520181e421302eb07c09aa02eebe8b8a0e4b30
M3hTi <mehdinasir502@yahoo.com>
2025-07-16 16:14:17 +0330

    ✨ add nested routes with Landing as index and Home under AppLayout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AppLayout from "./ui/AppLayout";
+import Landing from "./pages/Landing";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<AppLayout />}></Route>
+        <Route path="/" element={<AppLayout />}>
+          <Route index element={<Landing />} />
+          <Route path="/home" element={<Home />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
