e7f18ac823617ed79f2c1af6230d536cd0fd0b13
M3hTi <mehdinasir502@yahoo.com>
2025-07-17 12:31:23 +0330

    Initilize react-query

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,24 @@ import Landing from "./pages/Landing";
 import HomePage from "./pages/HomePage";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<AppLayout />}>
-          <Route index element={<Landing />} />
-          <Route path="/home" element={<HomePage />} />
-        </Route>
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<AppLayout />}>
+            <Route index element={<Landing />} />
+            <Route path="/home" element={<HomePage />} />
+          </Route>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
