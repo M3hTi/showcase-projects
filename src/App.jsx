142ba962fccaf98c6226a393295e5cc298a2b824
M3hTi <mehdinasir502@yahoo.com>
2025-07-17 12:03:49 +0330

    rename Home component to HomePage for clearer naming convention

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AppLayout from "./ui/AppLayout";
 import Landing from "./pages/Landing";
-import Home from "./pages/Home";
+import HomePage from "./pages/HomePage";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 
@@ -11,7 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<AppLayout />}>
           <Route index element={<Landing />} />
-          <Route path="/home" element={<Home />} />
+          <Route path="/home" element={<HomePage />} />
         </Route>
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
