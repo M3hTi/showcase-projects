445c721ec2b4ff08172ece5de2bb478082ac7165
M3hTi <mehdinasir502@yahoo.com>
2025-07-17 14:16:47 +0330

    update routes to use LoginPage and SignupPage components

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AppLayout from "./ui/AppLayout";
 import Landing from "./pages/Landing";
 import HomePage from "./pages/HomePage";
-import Signup from "./pages/Signup";
-import Login from "./pages/Login";
+import SignupPage from "./pages/SignupPage";
+import LoginPage from "./pages/LoginPage";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
@@ -17,8 +17,8 @@ function App() {
             <Route index element={<Landing />} />
             <Route path="/home" element={<HomePage />} />
           </Route>
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/login" element={<LoginPage />} />
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
