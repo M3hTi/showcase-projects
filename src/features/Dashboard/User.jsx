11ea0029bbed40270cb4aca51c45098829deb388
M3hTi <mehdinasir502@yahoo.com>
2025-07-18 18:22:36 +0330

    ♻️ move User component to Dashboard and add profile edit form with validation
    - moved User.jsx from authentication to Dashboard feature
    - integrated react-hook-form for profile editing
    - added validation and error display for fullname, email, password, and bio
    - added toast notifications on update success/failure
    - connected updateUserInfo mutation hook for updating user data

--- src/features/Dashboard/User.jsx
+++ src/features/Dashboard/User.jsx
@@ -1,14 +1,53 @@
 import Header from "../../ui/Header";
 import Button from "../../ui/Button";
-import { useUser } from "./useUser";
+import { useUser } from "../authentication/useUser";
 import { Link } from "react-router-dom";
-import { useLogout } from "./useLogout";
+import { useLogout } from "../authentication/useLogout";
 import MiniLoading from "../../ui/MiniLoading";
+import { useForm } from "react-hook-form";
+import Error from "../../ui/Error";
+import { useUpdateInfo } from "./useUpdateInfo";
+import toast from "react-hot-toast";
 
 function User() {
   const { user } = useUser();
 
   const { logOut, isPending } = useLogout();
+
+  const { updateUserInfo, updating } = useUpdateInfo();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({
+    defaultValues: {
+      email: user?.email,
+      fullname: user?.user_metadata?.fullname,
+      bio: user?.user_metadata?.bio
+    },
+  });
+
+  function submit(data) {
+    console.log(data);
+
+    const { email, password, fullname, bio } = data;
+
+    const newData = { email, password, data: { fullname, bio } };
+
+    updateUserInfo(newData, {
+      onSuccess: () => {
+        toast.success("Your information is updated!");
+        reset();
+      },
+
+      onError: (err) => {
+        toast.error(`${err.message}`);
+        reset();
+      },
+    });
+  }
 
   return (
     <div className="bg-gray-900">
@@ -54,7 +93,7 @@ function User() {
                   <h2 className="mb-4 text-xl font-semibold text-orange-500">
                     Edit Profile
                   </h2>
-                  <form className="space-y-4">
+                  <form className="space-y-4" onSubmit={handleSubmit(submit)}>
                     <div>
                       <label className="block text-sm font-medium text-gray-400">
                         Full Name
@@ -63,7 +102,13 @@ function User() {
                         type="text"
                         className="mt-1 w-full rounded-lg bg-gray-800 p-2 text-white focus:ring-2 focus:ring-orange-500 focus:outline-none"
                         placeholder="Enter your full name"
+                        {...register("fullname", {
+                          required: "Please Enter Your Full Name",
+                        })}
                       />
+                      {errors?.fullname?.message && (
+                        <Error>{errors.fullname.message}</Error>
+                      )}
                     </div>
                     <div>
                       <label className="block text-sm font-medium text-gray-400">
@@ -73,7 +118,41 @@ function User() {
                         type="email"
                         className="mt-1 w-full rounded-lg bg-gray-800 p-2 text-white focus:ring-2 focus:ring-orange-500 focus:outline-none"
                         placeholder="Enter your email"
+                        {...register("email", {
+                          required: "Please Enter Your Email",
+                          pattern: {
+                            value:
+                              /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                            message:
+                              "Please enter a valid email address. For example: user@example.com",
+                          },
+                        })}
                       />
+                      {errors?.email?.message && (
+                        <Error>{errors.email.message}</Error>
+                      )}
+                    </div>
+                    <div>
+                      <label className="block text-sm font-medium text-gray-400">
+                        Password
+                      </label>
+                      <input
+                        type="password"
+                        className="mt-1 w-full rounded-lg bg-gray-800 p-2 text-white focus:ring-2 focus:ring-orange-500 focus:outline-none"
+                        placeholder="Enter your password"
+                        {...register("password", {
+                          required: "Please Enter Your Email",
+                          pattern: {
+                            value:
+                              /^(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).{8,16}$/,
+                            message:
+                              "Password must be 8–16 characters long and include at least one uppercase letter, one number, and one special character.",
+                          },
+                        })}
+                      />
+                      {errors?.password?.message && (
+                        <Error>{errors.password.message}</Error>
+                      )}
                     </div>
                     <div>
                       <label className="block text-sm font-medium text-gray-400">
@@ -83,7 +162,26 @@ function User() {
                         className="mt-1 w-full rounded-lg bg-gray-800 p-2 text-white focus:ring-2 focus:ring-orange-500 focus:outline-none"
                         placeholder="Tell us about yourself"
                         rows="4"
+                        {...register("bio", {
+                          required: "Please Enter Your Biography.",
+                          minLength: {
+                            value: 10,
+                            message:
+                              "Your Biograpgy must be at least 10 characters",
+                          },
+                        })}
                       />
+                      {errors?.bio?.message && (
+                        <Error>{errors.bio.message}</Error>
+                      )}
+                    </div>
+                    <div className="mt-6 flex justify-end">
+                      <button
+                        type="submit"
+                        className="cursor-pointer rounded-lg bg-orange-500 px-6 py-3 font-medium text-white transition-all hover:bg-orange-600"
+                      >
+                        Save Changes
+                      </button>
                     </div>
                   </form>
                 </div>
@@ -98,7 +196,7 @@ function User() {
                   </Button>
                   <Button
                     onClick={() => logOut()}
-                    className="rounded-lg cursor-pointer bg-orange-500 px-6 py-3 font-medium text-white transition-all hover:bg-orange-600"
+                    className="cursor-pointer rounded-lg bg-orange-500 px-6 py-3 font-medium text-white transition-all hover:bg-orange-600"
                   >
                     {isPending ? <MiniLoading /> : " Sign Out"}
                   </Button>
