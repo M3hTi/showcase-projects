23a87dc93d35c0d1447a9850d2d133b845c81eed
M3hTi <mehdinasir502@yahoo.com>
2025-09-18 17:23:54 +0330

    integrate URL search params for technology filtering
    - Add `useSearchParams` to manage filter state via URL.
    - Implement `handleFilter` to update search params when a technology is selected or cleared.
    - Update `onChange` to use `handleFilter` for setting both local state and URL params.
    - Add debug logs for filter selection state.

--- src/ui/Filter.jsx
+++ src/ui/Filter.jsx
@@ -1,9 +1,9 @@
-
-
+import { useSearchParams } from "react-router-dom";
 
 function Filter({ projects, filterTechnology, setfilterTechnology }) {
-//   const { filterTechnology, setFilterTechnology } = useTechnologyFilter();
+  //   const { filterTechnology, setFilterTechnology } = useTechnologyFilter();
 
+  const [searchParams, setSearchParams] = useSearchParams();
 
   const uniqueTechnologies = [
     ...new Set(
@@ -14,12 +14,37 @@ function Filter({ projects, filterTechnology, setfilterTechnology }) {
   ];
 
   console.log(uniqueTechnologies);
+
+  function handleFilter(e) {
+    if (e.target.value) {
+      console.log(
+        "%c🔍 DEBUG: You choose Filter:",
+        "color: #8B5CF6; font-weight: bold",
+        true,
+      );
+      searchParams.set("filterBy", e.target.value);
+
+      setSearchParams(searchParams);
+    } else {
+      console.log(
+        "%c🔍 DEBUG: You choose filter: ",
+        "color: #8B5CF6; font-weight: bold",
+        false,
+      );
+      searchParams.delete("filterBy");
+
+      setSearchParams(searchParams);
+    }
+
+    setfilterTechnology(e.target.value);
+  }
+
   return (
     <div>
       <select
         name="technologies"
         value={filterTechnology}
-        onChange={(e) => setfilterTechnology(e.target.value)}
+        onChange={handleFilter}
         className="w-full cursor-pointer rounded-lg border border-gray-700 bg-gray-800/40 px-3 py-1.5 text-sm text-white backdrop-blur-sm transition-all hover:border-gray-600 focus:border-orange-500 focus:ring-2 focus:ring-orange-500/50 focus:outline-none sm:w-auto sm:px-4 sm:py-2 sm:text-base"
       >
         <option value="" className="bg-gray-800">
